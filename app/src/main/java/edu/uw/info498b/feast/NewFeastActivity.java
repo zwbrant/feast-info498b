8a8a80faf8886cd68b1bf4aa0497e1d1c26fbe23
C Nicholas Mao <mao@tacktechnologies.com>
2016-05-23 19:11:50 -0700

    added new feast activity

---
+++ app/src/main/java/edu/uw/info498b/feast/NewFeastActivity.java
@@ -0,0 +1,18 @@
+package edu.uw.info498b.feast;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Nick on 5/23/16.
+ */
+public class NewFeastActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_new_feast);
+
+    }
+}
