136e7c4f0c14518efb2cf23b4ba6d1aa4cab244b
C Nicholas Mao <mao@tacktechnologies.com>
2016-05-30 16:37:47 -0700

    added receiver and send service

---
+++ app/src/main/java/edu/uw/info498b/feast/SMSSendService.java
@@ -0,0 +1,39 @@
+package edu.uw.info498b.feast;
+
+import android.app.IntentService;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+
+/**
+ * Created by Nick on 5/30/2016.
+ * Service intent to handle sending messages.
+ */
+public class SMSSendService extends IntentService {
+
+    private static final int SMS_SENT_CODE = 2;
+
+    public static final String ACTION_SMS_STATUS = "edu.uw.info498b.feast.ACTION_SMS_STATUS";
+
+    //-----------------------//
+    //   O V E R R I D E S   //
+    //-----------------------//
+
+    public SMSSendService() {
+        super(SMSSendService.class.getName());
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent.getAction().equals(ACTION_SMS_STATUS)) {
+            Intent broadcastIntent = new Intent(ACTION_SMS_STATUS);
+            PendingIntent pendingIntent = PendingIntent.getBroadcast(this, SMS_SENT_CODE, broadcastIntent, 0);
+            Bundle extra = intent.getExtras();
+            String number = extra.getString("number");
+            String message = extra.getString("message");
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(number, null, message, pendingIntent, null);
+        }
+    }
+}
